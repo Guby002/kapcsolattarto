690cbc5eb841fe0113e72b42099a36a92f55d12e
Ferenc <guby002@gmail.com>
2022-03-11 12:45:19 +0100

    create Entity,some DTO, and Mapper
    Write some test for mapper

--- src/main/java/hu/futureofmedia/task/contactsapi/entities/Company.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/entities/Company.java
@@ -1,11 +1,14 @@
 package hu.futureofmedia.task.contactsapi.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Company {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "company_generator")
     private Long id;
     private String name;
 
