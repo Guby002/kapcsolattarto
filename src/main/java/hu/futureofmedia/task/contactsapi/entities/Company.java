680559447188d2a18d173c13139d7cf1945cfb7e
Guby002 <guby002@gmail.com>
2022-03-21 06:24:31 +0100

    create first task with test and some other

--- src/main/java/hu/futureofmedia/task/contactsapi/entities/Company.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/entities/Company.java
@@ -1,16 +1,26 @@
 package hu.futureofmedia.task.contactsapi.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Company {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "company_generator")
     private Long id;
     private String name;
+
 
     public Long getId() {
         return id;
