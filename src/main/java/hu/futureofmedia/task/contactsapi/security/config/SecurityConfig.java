b7ea207eb2037cafe596f037617c2821bb2b9c49
Ferenc <guby002@gmail.com>
2022-04-13 11:48:29 +0200

    add swgger config #2

---
+++ src/main/java/hu/futureofmedia/task/contactsapi/security/config/SecurityConfig.java
@@ -0,0 +1,81 @@
+package hu.futureofmedia.task.contactsapi.security.config;
+
+import hu.futureofmedia.task.contactsapi.entities.RoleName;
+import hu.futureofmedia.task.contactsapi.security.AuthEntryPointJwt;
+import hu.futureofmedia.task.contactsapi.security.Encoder;
+import hu.futureofmedia.task.contactsapi.security.JwtTokenFilter;
+import hu.futureofmedia.task.contactsapi.security.JwtTokenUtil;
+import hu.futureofmedia.task.contactsapi.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    //FOR SWAGGER
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/rest-api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/rest-api-docs/swagger-config/**"
+            // other public endpoints of your API may be appended to this array
+    };
+
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthEntryPointJwt unauthorizedHandler;
+    private final Encoder encoder;
+
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(encoder.passwordEncoder());
+    }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                //PUBLIC ENDPOINTS
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .antMatchers("/api/auth/login","/api/auth/register").permitAll()
+                //PRIVATE ENDPOINTS
+                .antMatchers(HttpMethod.GET, "/api/contact/foruser/**").hasAnyAuthority(RoleName.USER.name(),RoleName.ADMIN.name())
+                .antMatchers(HttpMethod.PUT, "/api/contact/**").hasAnyAuthority(RoleName.ADMIN.name(),RoleName.USER.name())
+                .antMatchers(HttpMethod.DELETE, "/api/contact/**").hasAuthority(RoleName.ADMIN.name())
+                .antMatchers(HttpMethod.POST, "/api/contact").hasAuthority(RoleName.ADMIN.name())
+                .antMatchers(HttpMethod.GET, "/api/contact/user/**").hasAuthority(RoleName.ADMIN.name())
+                .anyRequest().authenticated();
+        http.addFilterBefore(new JwtTokenFilter(jwtTokenUtil, userDetailsService), UsernamePasswordAuthenticationFilter.class);
+    }
+}
